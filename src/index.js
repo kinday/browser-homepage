03b606a692b4acf5c502a9b28e74b3d309c0ad82
Leonard Kinday <leonard@kinday.ru>
2021-12-11 00:19:50 +0100

    Search over multiple lists

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 const searchInput = document.querySelector("#search")
-const docLinksList = document.querySelector("#docs")
+const lists = document.querySelectorAll(".js-searchable")
 
 if (searchInput) {
   searchInput.addEventListener("input", () => {
     const value = searchInput.value
 
-    for (const child of docLinksList.children) {
-      child.classList.remove("hidden")
+    console.log(lists)
+    for (const list of lists) {
+      console.log(list)
+      for (const child of list.children) {
+        child.classList.remove("hidden")
 
-      if (child.textContent.toLowerCase().indexOf(value.toLowerCase()) < 0) {
-        child.classList.add("hidden")
+        if (child.textContent.toLowerCase().indexOf(value.toLowerCase()) < 0) {
+          child.classList.add("hidden")
+        }
       }
     }
   })
